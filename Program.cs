15ea37b0750b1972c5a2b3f14f91e7c811858281
fiercemaven <katreenmonique@byui.edu>
2022-01-13 16:43:26 -0700

    initialized Tictactoe project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Tictactoe
+{
+    public static class Program {
+       public static void Main(string[] args){
+          int myNum = 1; 
+         Console.WriteLine($"my number is {myNum}");
+         Console.WriteLine("Hello, Kat!");
+
+       }
+    }
+}
